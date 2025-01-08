21b3a2e83d785f50d750956f9538167cccd4a976
siri123 <saisirishayarra692@gmail.com>
2025-01-08 09:55:07 +0530

    code

--- 50-eks/main.tf
+++ 50-eks/main.tf
@@ -1,7 +1,5 @@
 resource "aws_key_pair" "eks" {
   key_name   = "eks"
-  # you can paste the public key directly like this
-  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
   public_key = file("eks.pub")
 }
 
--- 80-ecr/main.tf
+++ 80-ecr/main.tf
@@ -1,5 +1,14 @@
+resource "aws_ecr_repository" "mysql" {
+  name                 = "app/mysql"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "aws_ecr_repository" "backend" {
-  name                 = "expense/backend"
+  name                 = "app/backend"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
@@ -7,13 +16,13 @@ resource "aws_ecr_repository" "backend" {
   }
 }
 
-resource "aws_ecr_repository" "frontend" {
-  name                 = "expense/frontend"
-  image_tag_mutability = "MUTABLE"
+# resource "aws_ecr_repository" "frontend" {
+#   name                 = "expense/frontend"
+#   image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
+#   image_scanning_configuration {
+#     scan_on_push = true
+#   }
+# }
 
 
